9eed604803d9a42450033801e72d34a526a6a47a
David Henriquez <david.henriquez.23@gmail.com>
2020-01-19 11:56:15 -0500

    ADD last changes

--- src/main/resources/SchemaChanges.sql
+++ src/main/resources/SchemaChanges.sql
@@ -3,12 +3,10 @@ ALTER TABLE antecedente DROP FOREIGN KEY FK7rcc74x1dq1kg65oc1qkscs19;
 ALTER TABLE antecedente DROP COLUMN id_es_la_primera_hospitalizacion;
 ALTER TABLE antecedente ADD COLUMN causa VARCHAR(255);
 
-USE rehabilicop;
 ALTER TABLE farmacologico DROP FOREIGN KEY FKkcm18900vo851wxv93nv48h5o;
 ALTER TABLE farmacologico DROP COLUMN id_tiempo_de_uso;
 ALTER TABLE farmacologico ADD COLUMN tiempo_de_uso VARCHAR(255);
 
-USE rehabilicop;
 ALTER TABLE farmacologico DROP FOREIGN KEY FK3kq3r2wsqgwq49ue3jww4g77w;
 ALTER TABLE farmacologico DROP COLUMN id_adverso;
 ALTER TABLE farmacologico ADD COLUMN evento_adverso VARCHAR(255);
